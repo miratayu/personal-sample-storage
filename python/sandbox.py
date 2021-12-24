380b2890c565c17530258edfa79699f3c91f022a
miratayu <54067490+miratayu@users.noreply.github.com>
2021-12-24 13:27:47 +0900

    Update

--- python/sandbox.py
+++ python/sandbox.py
@@ -28,12 +28,48 @@ def join_items(data: dict) -> None:
     logger.info(result)
 
 
-if __name__ == '__main__':
-    logger.info('sandbox')
-    with open('contents_list.json') as load_file:
+def load_json_file(file_name: str = 'contents_list.json') -> None:
+    with open(file_name) as load_file:
         test_file = json.load(load_file)
     logger.info(f'test_file: {test_file}')
     for index, current in enumerate(test_file['list']):
         logger.info(f'index: {index}, current: {current}')
         logger.info(data_info(current, 'title'))
         join_items(current['item'])
+
+
+def save_text(text: str = "") -> None:
+    if not text:
+        logger.info(f'None')
+    if text:
+        text += "-"
+    result_text = text + "result.txt"
+    logger.info(f'{result_text}')
+
+
+def add(a, b):
+    return a+b
+
+
+def test1(func):
+    logger.info(f'{type(func)}')  # <class 'function'>
+    return func(1, 2)
+
+
+def open_step() -> None:
+    logger.info('step1')
+    logger.info('step2')
+    logger.info('step3')
+
+
+def run_open(func) -> None:
+    logger.info('run_open')
+    func()
+
+
+if __name__ == '__main__':
+    logger.info('sandbox')
+
+    logger.info(f'test1: {test1(add)}')
+    run_open(open_step)
+
