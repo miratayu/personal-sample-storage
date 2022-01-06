0e79436f26867fcd623a6d7457bbdc483450ba6a
miratayu <54067490+miratayu@users.noreply.github.com>
2022-01-06 12:57:34 +0900

    Update test_sandbox.py

--- python/test_sandbox.py
+++ python/test_sandbox.py
@@ -1,6 +1,8 @@
 import pytest
 import logging
 
+from _pytest.fixtures import FixtureRequest
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -8,8 +10,8 @@ logger = logging.getLogger(__name__)
 class TestSandBox:
 
     @pytest.mark.SandBox
-    def test_sand_box(self, request):
+    def test_sand_box(self, request: FixtureRequest):
         logger.info(f'request: {request}')
         logger.info(f'type(request): {type(request)}')
-        pass
+        logger.info(f'test name: {request.node.name}')
 
