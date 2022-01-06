b0a800c8f08a6db728ae6f845ba751079f04d59b
miratayu <54067490+miratayu@users.noreply.github.com>
2022-01-06 14:13:13 +0900

    Update test_sandbox.py

--- python/test_sandbox.py
+++ python/test_sandbox.py
@@ -9,9 +9,21 @@ logger = logging.getLogger(__name__)
 
 class TestSandBox:
 
+    @staticmethod
+    def _markers(request: FixtureRequest):
+        return [x.name for x in request.node.iter_markers() if "fixture" not in x.name]
+
+    def _id(self, request: FixtureRequest):
+        markers = self._markers(request)
+        logger.info(f'markers: {markers}')
+        ids = [marker for marker in markers if marker.lower().startswith('id')]
+        return ids[0] if ids else ''
+
     @pytest.mark.SandBox
+    @pytest.mark.id_001
     def test_sand_box(self, request: FixtureRequest):
         logger.info(f'request: {request}')
         logger.info(f'type(request): {type(request)}')
         logger.info(f'test name: {request.node.name}')
-
+        logger.info(f'self._markers(request: {self._markers(request)}')
+        logger.info(f'self._id(request): {self._id(request)}')
