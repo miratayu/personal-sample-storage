ef9a39bb3c3a02d7a495be18f7a46243d3f8c964
miratayu <54067490+miratayu@users.noreply.github.com>
2022-05-11 14:05:23 +0900

    Add sandbox.groovy

---
+++ groovy/sandbox.groovy
@@ -0,0 +1,14 @@
+
+class Example {
+    static void sum(int a,int b) {
+        int c = a+b
+        print "Hello" as java.lang.Object
+        println c as java.lang.Object
+    }
+
+    static void main(String[] args) {
+        sum(10,5)
+    }
+}
+
+Example.main()
