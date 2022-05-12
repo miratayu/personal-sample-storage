59dfa59bec3ef17096381010e3aed8902c8d1de4
miratayu <54067490+miratayu@users.noreply.github.com>
2022-05-12 21:33:52 +0900

    Update

--- groovy/sandbox.groovy
+++ groovy/sandbox.groovy
@@ -2,12 +2,23 @@
 class Example {
     static void sum(int a,int b) {
         int c = a+b
-        print "Hello" as java.lang.Object
+        println "Hello" as java.lang.Object
         println c as java.lang.Object
+    }
+
+    static void say(first,second) {
+        if(!first) {
+            println "first string is empty!" as java.lang.Object
+            return
+        }
+        String result = "say!" + (first as java.lang.CharSequence) + (second as java.lang.CharSequence)
+        println result as java.lang.Object
     }
 
     static void main(String[] args) {
         sum(10,5)
+        // say("Hello" as java.lang.Object, "World" as java.lang.Object)
+        say("ABC" as java.lang.Object, "World" as java.lang.Object)
     }
 }
 
