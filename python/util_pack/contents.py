b9936247534d0dcbc7679525398d94369d4cdd76
miratayu <54067490+miratayu@users.noreply.github.com>
2022-06-07 13:05:51 +0900

    Experiment contents

--- python/util_pack/contents.py
+++ python/util_pack/contents.py
@@ -9,16 +9,17 @@ logger = logging.getLogger(__name__)
 def load() -> None:
     """ load """
     logger.info(f"load contents")
-    file_path = "resource/contents/"
+    file_path = "resource\\contents"
     file_pattern = r"*.json"
     result = {}
-    contents_files = glob.glob(file_path + file_pattern)
+    contents_files = glob.glob(file_path + "\\" + file_pattern)
     logger.info(f"contents_files: {contents_files}")
     for current_file in contents_files:
         logger.info(f"current_file: {current_file}")
         with open(current_file, "r") as load_file:
             load_contents = json.load(load_file)
-            result_key = current_file.replace("resource/contents\\contents_", "").replace(".json", "")
+            result_key = current_file.replace(".json", "").replace(f"{file_path}", "").replace("\\contents_", "")
+            logger.info(f"result_key: {result_key}")
             result[f"{result_key}"] = load_contents
     logger.info(f"resul: {result}")
     with open('resource/output/contents_result_list.json', 'w') as file:
