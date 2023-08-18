03a1bd0a1e75e50e50f5be75736918789fe46128
miratayu <54067490+miratayu@users.noreply.github.com>
2023-08-18 22:08:48 +0900

    Add Sandbox.checkType

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -11,4 +11,9 @@ static void main(String[] args) {
     println "Hello world!"
     println Sample.add(1, 2)
     println Sandbox.sampleOne()
+    println Sandbox.checkType("test")
+    println Sandbox.checkType(1)
+    println Sandbox.checkType(true)
+    println Sandbox.checkType([])
+    println Sandbox.checkType([:])
 }
--- src/main/groovy/org/example/Sandbox.groovy
+++ src/main/groovy/org/example/Sandbox.groovy
@@ -9,4 +9,23 @@ class Sandbox {
         def name = object.name
         println name
     }
+    static def checkType(value) {
+        println value.getClass().getSimpleName()
+        if (value instanceof String) {
+            return "str"
+        }
+        if (value instanceof Integer) {
+            return "int"
+        }
+        if (value instanceof Boolean) {
+            return "bool"
+        }
+        if (value instanceof List) {
+            return "list"
+        }
+        if (value instanceof HashMap) {
+            return "hashMap"
+        }
+        return "none"
+    }
 }
