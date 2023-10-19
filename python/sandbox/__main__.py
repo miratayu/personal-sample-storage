8de9cc4b5b12381eb2f55f5a2103796fea954603
miratayu <54067490+miratayu@users.noreply.github.com>
2023-10-19 20:35:22 +0900

    add check python version

--- python/sandbox/__main__.py
+++ python/sandbox/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 
 import sandbox
@@ -7,6 +8,8 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
+    logger.info(f"python version: {sys.version_info}")
+    logger.info(f"isPythonVersionOver3.8: {sys.version_info>=(3, 8)}")
     logger.info("sandbox main")
     # sample_runs()
     # inequality_sign()
